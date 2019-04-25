2fca218af0b6fa79399969b07f71067ceaa8e94e
Rodolfo Quispe <quispe@sliv.ic.unicamp.br>
2019-04-25 17:55:31 +0000

    Added data for test 1

--- precision_recall.py
+++ precision_recall.py
@@ -64,22 +64,57 @@ def precision_recall(distmat, q_ids, g_ids, max_rank = 20, gt_samples = 20):
   return precision, recall
 
 if __name__ == '__main__':
-  ids, names, ms_features, ss_features = load_data("MO445-descriptors/examples/names.txt", "MO445-descriptors/examples/mpeg7_features/")
+  #ids, names, ms_features, ss_features = load_data("MO445-descriptors/examples/names.txt", "MO445-descriptors/examples/mpeg7_features/")
+  ids = np.array([0, 0, 0 , 0, 0, 1, 1, 1, 1, 1])
+  features_1 = np.array([[1.5, 2.5],
+                      [1.5, 2.0],
+                      [2.0, 2.0],
+                      [1.0, 2.0],
+                      [1.5, 1.5],
+                      [1.0, 1.0],
+                      [1.0, 2.0],
+                      [1.0, 3.0],
+                      [1.0, 4.0],
+                      [1.0, 5.0]])
 
-  max_rank = 1400
-  dismat_MS = dist(ms_features, ms_features)
-  pms, rms = precision_recall(dismat_MS, ids, ids, max_rank=max_rank)
+  features_2 = np.array([[2, 1],
+                      [2.0, 2.0],
+                      [2.0, 3.0],
+                      [2.0, 4.0],
+                      [2.0, 5.0],
+                      [1.4, 1.4],
+                      [1.6, 1.4],
+                      [1.6, 1.2],
+                      [1.4, 1.2],
+                      [1.5, 1.3]])
 
-  dismat_SS = dist(ss_features, ss_features)
-  pss, rss = precision_recall(dismat_SS, ids, ids, max_rank=max_rank)
+  features_3 = np.array([[1.5, 2.5],
+                      [1.5, 2.0],
+                      [1.75, 2.25],
+                      [1.25, 2.0],
+                      [1.5, 1.5],
+                      [1.5, 5.5],
+                      [1.25, 5.0],
+                      [1.5, 5.0],
+                      [1.15, 5.0],
+                      [1.5, 4.5]])
+
+  max_rank = 10
+  dismat_1 = dist(features_1, features_1)
+  dismat_2 = dist(features_2, features_2)
+  dismat_3 = dist(features_3, features_3)
+  p_1, r_1 = precision_recall(dismat_1, ids, ids, max_rank=max_rank, gt_samples=5)
+  p_2, r_2 = precision_recall(dismat_2, ids, ids, max_rank=max_rank, gt_samples=5)
+  p_3, r_3 = precision_recall(dismat_3, ids, ids, max_rank=max_rank, gt_samples=5)
 
   print("Generating Precision x Recall curve")
-  plt.title('Methods comparison - MO805 - Assignment 7')
-  plt.plot(rms, pms, label = 'Multiscale fractal dimension')
-  plt.plot(rss, pss, label = 'Segment saliences')
-  plt.legend(loc = 'upper right')
-  plt.xlim([0, 1])
-  plt.ylim([0, 1])
+  plt.title('MO805 - Test 1 - question 4')
+  plt.plot(r_1, p_1, label = 'Descriptor 1')
+  plt.plot(r_2, p_2, label = 'Descriptor 2')
+  plt.plot(r_3, p_3, label = 'Descriptor 3')
+  plt.legend(loc = 'lower left')
+  plt.xlim([0, 1.01])
+  plt.ylim([0, 1.01])
   plt.ylabel('precision')
   plt.xlabel('recall')
   plt.savefig("precision_recall.png")
